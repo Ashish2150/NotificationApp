0717e3ae6c19589faf6ad649fc6fd58dc0a752c9
Ashish2150 <ashishmauryait@gmail.com>
2018-01-30 23:38:06 +0530

    Initial Commit with normal notification working in swift 4.

--- NotificationApp/ViewController.swift
+++ NotificationApp/ViewController.swift
@@ -7,18 +7,92 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound], completionHandler: {(grant,error) in
+            
+            if grant{
+                print("Request Granted for Notification.")
+                
+            } else {
+                print("Reject request of Notification.")
+            }
+            
+        })
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK:- Notifiy Button Click -
+    
+    @IBAction func notifyButtonTabbed(_ sender: Any) {
+        
+        scheduleNotification(inSecond: 2, completion: { success in
+            
+            if success {
+                print("Notification Send Successfully")
+                
+            } else {
+                print("Notificatin not send")
+            }
+            
+        })
+        
     }
+    
+    
+    
+    
+    func scheduleNotification(inSecond: TimeInterval, completion: @escaping (_ Success: Bool)->()){
+        
+        //Add attachment...
+        let myImage = "Ashish"
+        guard let imgUrl = Bundle.main.url(forResource: myImage, withExtension: "jpg") else {
+            completion(false)
+            return
+        }
+
+        
+        var attachment: UNNotificationAttachment
+        attachment = try! UNNotificationAttachment(identifier: "MyNotification", url: imgUrl, options: .none)
+        
+        let notify = UNMutableNotificationContent()
+        notify.title = "First Notification"
+        notify.subtitle = "This notification is demo purpose."
+        notify.body = "The new notification is iOS version 10 only available"
+        notify.attachments = [attachment]
+        
+        
+        
+        let notifyTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSecond, repeats: false)
+        let request = UNNotificationRequest(identifier: "MyNotification", content: notify, trigger: notifyTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            
+            if error != nil {
+                
+                print(error ?? "Error in send notification")
+                completion(false)
+                
+            } else {
+                completion(true)
+            }
+            
+            
+        })
+
+        
+        
+        
+        
+    }
+    
+    
+    
 
 
 }
